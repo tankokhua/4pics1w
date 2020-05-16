652a2561335d75364d865e50bc0010a36935f283
tankokhua <tankokhua@outlook.com>
2020-05-16 19:11:34 +0800

    - Convert from webapp2 to flask, change runtime to Python37.

--- main.py
+++ main.py
@@ -14,65 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from flask import Flask, render_template, request
 import jinja2
-import webapp2
 import os
 import logging
 import re
 
-jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+app = Flask(__name__)
 
-class HintsHandler(webapp2.RequestHandler):
-    def post(self):
-        wordlength = self.request.get("wordlength").strip()
-        letters    = self.request.get("letters").lower().replace(' ','',12)
-        filters    = self.request.get("filters").lower().replace(' ','',12)
-        answers = "No valid answers."
-        if re.search(r'\d', wordlength) and 3<=int(wordlength)<=9:
-           try:
-              words = " ".join(open("w%s" % wordlength).readlines()).split()
-              filterlist = ''.join(set(letters))
+#jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+@app.route('/gethints', methods=['GET', 'POST'])
+def HintsHandler():
+    _wordlength = request.form.get("wordlength").strip()
+    _letters    = request.form.get("letters").lower().replace(' ', '', 12)
+    _filters    = request.form.get("filters").lower().replace(' ', '', 12)
+    _answers = "No valid answers."
+    if re.search(r'\d', _wordlength) and 3<=int(_wordlength)<=9:
+       try:
+          words = " ".join(open("w%s" % _wordlength).readlines()).split()
+          filterlist = ''.join(set(_letters))
    
-              cnt=0
-              answers = "<b>Possible answers are:</b><br>"
-              for word in words:
-                  if not re.search(r'[^%s]' % filterlist, word):
-                     skip = False
-                     for ch in filterlist:
-              	         if word.count(ch)>letters.count(ch):
-              	            skip = True
-              	            break
-                     if skip:
-              	        continue
-                     if len(filters)==int(wordlength) and not re.search(r'%s' % filters, word):
-                        continue
-                     cnt+=1
-                     answers += " %s" % word
-              if cnt==0:
-                 answers = "No valid answers."
-           except:
-              answers = "Error executing scrpt."
-        else:
-           answers = "Wordlength must be between 3 to 9."
+          cnt=0
+          _answers = ""
+          for word in words:
+              if not re.search(r'[^%s]' % filterlist, word):
+                 skip = False
+                 for ch in filterlist:
+                   if word.count(ch)>_letters.count(ch):
+                      skip = True
+                      break
+                 if skip:
+                    continue
+                 if len(_filters)==int(_wordlength) and not re.search(r'%s' % _filters, word):
+                    continue
+                 cnt+=1
+                 _answers += " %s" % word
+          if cnt==0:
+             _answers = "No valid answers."
+       except:
+          _answers = "Error executing scrpt."
+    else:
+       _answers = "Wordlength must be between 3 to 9."
 
-        logging.info("wordlength %s [%s] [%s]" % (wordlength, letters, answers))
-        template_values = {
-                           "answers":answers
-                          }
-        template = jinja_environment.get_template('templates/answers.htm')
-        self.response.out.write(template.render(template_values))
+    logging.info("wordlength %s [%s] [%s]" % (_wordlength, _letters, _answers))
+    return render_template("answers.htm", answers=_answers)
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        template_values = {
-                             "wordlength":"",
-                             "letters":"",
-                             "filters":"",
-                          }
-        template = jinja_environment.get_template('templates/index.htm')
-        self.response.out.write(template.render(template_values))
+@app.route('/', methods=['GET', 'POST'])
+def MainHandler():
+    return render_template("index.htm", wordlength="", letters="", filters="")
 
-app = webapp2.WSGIApplication([
-                                ('/', MainHandler),
-                                ('/gethints', HintsHandler),
-                              ], debug=True)
+if __name__ == '__main__':
+   app.run(host='127.0.0.1', port=8080, debug=True)
--- readme.txt
+++ readme.txt
@@ -7,3 +7,16 @@ Notes:
 1. In 'app.yaml', remove 'application' and 'version' keywords.
 2. Enable Cloud Build api
    https://console.developers.google.com/apis/api/cloudbuild.googleapis.com/overview?project=4pics1w
+
+
+o. Powershell (run as adminstrator)
+   o. Outside the project directory:
+      % > python -m venv env
+      % > env/Scipts/activate
+   o. Inside project Directory
+      - Deploy to server
+        %> gcloud app deploy
+
+      - Run locally
+        %> pip install -r requirements.txt
+        %> python .\main.py
